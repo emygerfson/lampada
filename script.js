8bd01392578a574cb9f96c2e07720ebebace8497
emygerfson <emy.silva1404@gmail.com>
2025-05-01 18:31:52 -0300

    primeiro comentario

---
+++ script.js
@@ -0,0 +1,14 @@
+const ligar = document.getElementById('ligar');
+const desligar = document.getElementById('Desligar');
+const lampada = document.getElementById('lampada');
+
+function ligarLampada(){
+    lampada.src = "/imagem/lampada-acesa.png";
+    
+}
+function desligarLampada(){
+    lampada.src = "imagem/lampada-apagada.png";
+}
+
+ligar.addEventListener('click', ligarLampada);
+desligar.addEventListener('click', desligarLampada);
